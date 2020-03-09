ad1894b74beb78be61260b67002896f87158d8ce
ninthDevilHAUNSTER <luotianhan@126.com>
2020-03-09 17:06:12 +0800

    3/9 下午场(7题) 有些题目有点水，关于前序后序建树的问题晚上再研究。

--- PAT/A1124 Raffle for Weibo Followers.cpp
+++ PAT/A1124 Raffle for Weibo Followers.cpp
@@ -17,13 +17,35 @@
 #include <queue>
 #include <cassert>
 #include <cmath>
+#include <unordered_map>
 
 using namespace std;
 #define INF 0x3fffffff
 #define inf 0x3fffffff
 
-int pat_solver() {
+int M, N, S; // total number of forwards , skip number of winners , index of 1st winner
 
+unordered_map<string, int> RaffleList;
+
+int pat_solver() {
+    scanf("%d%d%d", &M, &N, &S);
+    bool isOutput = false;
+    string name;
+    int real_i = 1;
+    for (int i = 1; i <= M; ++i) {
+        cin >> name;
+        if (real_i - S >= 0 && (real_i - S) % N == 0) {
+            if (RaffleList.find(name) == RaffleList.end()) {
+                RaffleList[name] = 1;
+                isOutput = true;
+                cout << name << endl;
+            } else {
+                continue;
+            }
+        }
+        real_i++;
+    }
+    if (!isOutput) cout << "Keep going...";
     return 0;
 }
 
